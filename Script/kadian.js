c2985ceeba600e52a27b8e2230aff76633182326
IWandersky <37397537+IWandersky@users.noreply.github.com>
2019-10-25 17:35:54 +0800

    Update kadian.js

--- Script/kadian.js
+++ Script/kadian.js
@@ -7,10 +7,10 @@ var url = $request.url;
 const path = "/api/v1.user/info";
 let obj = JSON.parse(body);
 if (url.indexOf(path) != -1) {
-	obj["isvip"] = "1";
-  obj["vip_end_time"] = "1666627200";
-  obj["vip_grade"] = "2";
-  obj["vip_type"] = "2";
+	obj["is_vip"] = "1";
+  	obj["vip_end_time"] = "1666627200";
+  	obj["vip_grade"] = "2";
+	obj["vip_type"] = "2";
 	body = JSON.stringify(obj);
  }
 $done({body});
