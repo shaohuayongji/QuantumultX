6905a0f94d54b33d29c7de5f0b60c3151962c9f1
IWandersky <37397537+IWandersky@users.noreply.github.com>
2019-10-26 17:48:12 +0800

    Update asmr.js

--- Script/asmr.js
+++ Script/asmr.js
@@ -6,12 +6,12 @@ const path1 = "/video/api/user/info";
 const path2 = "/video/api/login/check";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	obj["vip"] = "3270511028";
+	obj.data.user.vip = "3270511028";
 	body = JSON.stringify(obj);
  }
 if (url.indexOf(path2) != -1) {
 	let obj = JSON.parse(body);
-	obj["vip"] = "3270511028";
+	obj.data.user.vip = "3270511028";
 	body = JSON.stringify(obj);
  }
 $done({body});
