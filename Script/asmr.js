378647985a36bc46425cfb7fd0d4dbf480900d6f
IWandersky <37397537+IWandersky@users.noreply.github.com>
2019-10-26 15:22:42 +0800

    Update asmr.js

--- Script/asmr.js
+++ Script/asmr.js
@@ -3,23 +3,15 @@
 var body = $response.body;
 var url = $request.url;
 const path1 = "/video/api/user/info";
-const path2 = "/video/api/video/play";
-const path3 = "/video/api/charge/ioslist";
+const path2 = "/video/api/login/check";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	obj["vip"] = "1";
+	obj["vip"] = "3270511028";
 	body = JSON.stringify(obj);
  }
 if (url.indexOf(path2) != -1) {
 	let obj = JSON.parse(body);
-	obj["result"] = "100";
-	body = JSON.stringify(obj);
- }
-if (url.indexOf(path3) != -1) {
-	let obj = JSON.parse(body);
-	obj["result"] = "100";
-  	obj["days"] = "365";
-	obj["effective"] = "true";
+	obj["vip"] = "3270511028";
 	body = JSON.stringify(obj);
  }
 $done({body});
