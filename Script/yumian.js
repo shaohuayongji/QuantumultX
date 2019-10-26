254ff338cb86fa9e8e51f4573174c88437ebcc04
IWandersky <37397537+IWandersky@users.noreply.github.com>
2019-10-26 18:56:45 +0800

    Update yumian.js

--- Script/yumian.js
+++ Script/yumian.js
@@ -3,7 +3,9 @@ var url = $request.url;
 const path1 = "/api/sounds";
 if (url.indexOf(path1) != -1) {
 	let obj = JSON.parse(body);
-	obj.data["vip"] = "false";
+	for (i in obj.data) {
+	obj.data[i].vip = "false";
+	}
 	body = JSON.stringify(obj);
  }
 $done({body});
